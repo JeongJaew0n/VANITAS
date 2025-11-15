fab247bd9c1e4b74a21e47104b61ba264f25644b
HeLLo2 <popt0@naver.com>
2025-11-15 19:26:34 +0900

    chore: 모듈 초기화
    멀티 모듈 처음부터 해보기

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "vanitas" // 루트 프로젝트 이름 지정
 
 pluginManagement {
-    includeBuild("build-logic") // 사전 컴파일 컨벤션 플러그인을 프로젝트에 연결
     repositories {
         gradlePluginPortal()
         mavenCentral()
@@ -15,8 +14,3 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" // 필요한 JDK를 자동으로 준비
-}
-
-include("example") // Spring Boot 애플리케이션 모듈 등록
